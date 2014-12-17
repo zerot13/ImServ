e34973af6dcee4c2b4579360c561e2b6fdf503b1
zerot13 <imac0.13@gmail.com>
2014-12-17 15:34:49 -0800

    Working Client
    Client is working properly

--- ImageBrowser/ImageBrowser/Form1.Designer.cs
+++ ImageBrowser/ImageBrowser/Form1.Designer.cs
@@ -30,49 +30,79 @@
         {
             this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
             this.dirBrowseButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.responseLabel = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.directoryLabel = new System.Windows.Forms.Label();
+            this.fileCountLabel = new System.Windows.Forms.Label();
+            this.fileProgressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dirBrowseButton
             // 
-            this.dirBrowseButton.Location = new System.Drawing.Point(13, 13);
+            this.dirBrowseButton.Location = new System.Drawing.Point(12, 38);
             this.dirBrowseButton.Name = "dirBrowseButton";
-            this.dirBrowseButton.Size = new System.Drawing.Size(86, 23);
+            this.dirBrowseButton.Size = new System.Drawing.Size(98, 23);
             this.dirBrowseButton.TabIndex = 0;
-            this.dirBrowseButton.Text = "Select Folder";
+            this.dirBrowseButton.Text = "Select Directory";
             this.dirBrowseButton.UseVisualStyleBackColor = true;
             this.dirBrowseButton.Click += new System.EventHandler(this.dirBrowseButton_Click);
             // 
-            // button1
+            // runButton
             // 
-            this.button1.Location = new System.Drawing.Point(13, 227);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.runButton.Location = new System.Drawing.Point(12, 83);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(98, 23);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Transfer Files";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
             // 
-            // responseLabel
+            // directoryLabel
             // 
-            this.responseLabel.AutoSize = true;
-            this.responseLabel.Location = new System.Drawing.Point(13, 208);
-            this.responseLabel.Name = "responseLabel";
-            this.responseLabel.Size = new System.Drawing.Size(33, 13);
-            this.responseLabel.TabIndex = 2;
-            this.responseLabel.Text = "Label";
+            this.directoryLabel.AutoSize = true;
+            this.directoryLabel.Location = new System.Drawing.Point(12, 9);
+            this.directoryLabel.Name = "directoryLabel";
+            this.directoryLabel.Size = new System.Drawing.Size(109, 13);
+            this.directoryLabel.TabIndex = 2;
+            this.directoryLabel.Text = "No directory specified";
+            // 
+            // fileCountLabel
+            // 
+            this.fileCountLabel.AutoSize = true;
+            this.fileCountLabel.Location = new System.Drawing.Point(12, 22);
+            this.fileCountLabel.Name = "fileCountLabel";
+            this.fileCountLabel.Size = new System.Drawing.Size(34, 13);
+            this.fileCountLabel.TabIndex = 3;
+            this.fileCountLabel.Text = "0 files";
+            // 
+            // fileProgressBar
+            // 
+            this.fileProgressBar.Location = new System.Drawing.Point(12, 112);
+            this.fileProgressBar.Name = "fileProgressBar";
+            this.fileProgressBar.Size = new System.Drawing.Size(260, 23);
+            this.fileProgressBar.TabIndex = 4;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 138);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(0, 13);
+            this.progressLabel.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.responseLabel);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.fileProgressBar);
+            this.Controls.Add(this.fileCountLabel);
+            this.Controls.Add(this.directoryLabel);
+            this.Controls.Add(this.runButton);
             this.Controls.Add(this.dirBrowseButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Transfer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +112,11 @@
 
         private System.Windows.Forms.FolderBrowserDialog folderBrowser;
         private System.Windows.Forms.Button dirBrowseButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label responseLabel;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label directoryLabel;
+        private System.Windows.Forms.Label fileCountLabel;
+        private System.Windows.Forms.ProgressBar fileProgressBar;
+        private System.Windows.Forms.Label progressLabel;
     }
 }
 
---
+++ ImageBrowser/ImageBrowser/LogManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImageBrowser
+{
+    class LogManager
+    {
+        public LogManager()
+        {
+            
+        }
+
+        public void Debug(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Error(string msg, Exception e)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine(e);
+        }
+
+        public void Error(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
