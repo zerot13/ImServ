def693e557050c9569b54e46c4bf0b9c4f05f062
zerot13 <imac0.13@gmail.com>
2014-12-16 16:06:09 -0800

    Added Client

---
+++ ImageBrowser/ImageBrowser/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ImageBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.dirBrowseButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.responseLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dirBrowseButton
+            // 
+            this.dirBrowseButton.Location = new System.Drawing.Point(13, 13);
+            this.dirBrowseButton.Name = "dirBrowseButton";
+            this.dirBrowseButton.Size = new System.Drawing.Size(86, 23);
+            this.dirBrowseButton.TabIndex = 0;
+            this.dirBrowseButton.Text = "Select Folder";
+            this.dirBrowseButton.UseVisualStyleBackColor = true;
+            this.dirBrowseButton.Click += new System.EventHandler(this.dirBrowseButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // responseLabel
+            // 
+            this.responseLabel.AutoSize = true;
+            this.responseLabel.Location = new System.Drawing.Point(13, 208);
+            this.responseLabel.Name = "responseLabel";
+            this.responseLabel.Size = new System.Drawing.Size(33, 13);
+            this.responseLabel.TabIndex = 2;
+            this.responseLabel.Text = "Label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.responseLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dirBrowseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.Button dirBrowseButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label responseLabel;
+    }
+}
+
---
+++ ImageBrowser/ImageBrowser/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ImageBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dirBrowseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = folderBrowser.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                MessageBox.Show(folderBrowser.SelectedPath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            
+            responseLabel.Text = folderBrowser.SelectedPath;
+
+            string[] fileList = GetImageFiles(folderBrowser.SelectedPath);
+
+            foreach (string file in fileList)
+            {
+                if (SendFileName(Path.GetFileName(file)))
+                {
+                    byte[] fileData = File.ReadAllBytes(file);
+                    SendFileData(fileData);
+                }
+            }
+        }
+
+        private bool SendFileName(string fileName)
+        {
+            string responseText = "";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("");
+            req.Method = "POST";
+
+            using (StreamWriter writer = new StreamWriter(req.GetRequestStream()))
+            {
+                writer.Write(fileName);
+                writer.Flush();
+                writer.Close();
+            }
+            using (HttpWebResponse httpResponse = (HttpWebResponse)req.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    responseText = reader.ReadToEnd();
+                }
+            }
+            return responseText == "OK";
+        }
+
+        private void SendFileData(byte[] fileData)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("");
+            req.Method = "POST";
+            using (Stream s = req.GetRequestStream())
+            {
+                s.Write(fileData, 0, fileData.Length);
+                s.Flush();
+            }
+
+            using (HttpWebResponse httpResponse = (HttpWebResponse)req.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    responseLabel.Text = reader.ReadToEnd();
+                }
+            }
+        }
+
+        private string[] GetImageFiles(string selectedPath)
+        {
+            string[] filters = new String[] { "*.jpg", "*.jpeg", "*.png", "*.gif", "*.tiff", "*.bmp", "*.raw" };
+            List<string> fileList = new List<string>();
+            foreach (string filter in filters)
+            {
+                fileList.AddRange(Directory.GetFiles(selectedPath, filter, SearchOption.AllDirectories));
+            }
+            return fileList.ToArray();
+        }
+    }
+}
